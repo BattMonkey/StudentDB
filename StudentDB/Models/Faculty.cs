239a870390128b943751c9bfc91184287b3a7dfa
BatMonkey <matt.bunke@gmail.com>
2018-09-30 13:14:52 -0500

    MVC Patterns and Practices tutorial

---
+++ StudentDB/Models/Faculty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDB.Models
+{
+    public class Faculty
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
